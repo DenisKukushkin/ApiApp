a9707c89de6583830af139aba5c4dbdace249e35
DenisKukushkin <denisoverhere@gmail.com>
2022-11-18 09:40:25 +0300

    Initial Commit

---
+++ ApiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApiApp
+//
+//  Created by Кукушкин Денис Сергеевич on 18.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
